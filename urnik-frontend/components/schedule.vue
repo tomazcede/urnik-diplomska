88003a90fdf1e7ab1519b01a251fe9ec2181a3c7
tomazcede <tomi.cede1@gmail.com>
2025-06-21 18:43:17 +0200

    Bootstrap, tailwind css

---
+++ urnik-frontend/components/schedule.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="w-full">
+    <div class="row">
+        <div class="col" v-for="day in days">
+            <div class="w-20 h-20" v-for="hour in hours">
+                {{hour}}
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+const days = [
+    'mon', 'tue', 'wen', 'thu', 'fri'
+]
+
+const hours = [
+    1, 2, 3, 4, 5, 6, 7
+]
+</script>
+
+<style scoped>
+
+</style>
--- urnik-frontend/nuxt.config.ts
+++ urnik-frontend/nuxt.config.ts
@@ -1,4 +1,12 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['bootstrap/dist/css/bootstrap.min.css'],
+  app: {
+    head: {
+      script: [
+        { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js' }
+      ]
+    }
+  }
 })
