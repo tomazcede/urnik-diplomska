d5bfb3c610de7d7e3d7aeb6048ec2f193c7c479d
tomazcede <tomi.cede1@gmail.com>
2025-06-22 12:54:48 +0200

    Schedules v users

--- urnik-api/app/Models/User.php
+++ urnik-api/app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function schedules() {
+        return $this->hasMany(Schedule::class);
+    }
 }
