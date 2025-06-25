36fb2b2c18dad5c929fa92a05b479c467c7bf286
tomazcede <tomi.cede1@gmail.com>
2025-06-25 20:42:19 +0200

    Styling, za mobile tut, črta ki se premika glede na uro

--- urnik-frontend/components/schedule.vue
+++ urnik-frontend/components/schedule.vue
@@ -18,25 +18,53 @@
         +
     </button>
   </div>
-  <div class="row">
-    <div class="col">
-      <div class="h-5 md:h-10" ></div>
-      <div class="h-10 md:h-20" v-for="hour in hours" :key="hour">
-          {{hour}}
-      </div>
-    </div>
-    <div class="col" v-for="day in days" :key="day">
-      <div class="h-5 md:h-10" >{{ $t(day) }}</div>
-      <div class="h-10 md:h-20" v-for="hour in hours" :key="hour">
-        <div class="row" v-if="schedule[day][hour] && schedule[day][hour].length">
-          <div class="col" v-for="event in schedule[day][hour]" :key="event.id">
-            {{ event.name }}
+  <div>
+    <table class="w-full md:w-[75%] mx-auto table-fixed border-collapse border border-gray-300 text-xs">
+      <thead>
+      <tr class="bg-gray-100">
+        <th v-if="!isMobile" class="border border-gray-300 p-2 w-20"></th>
+        <th
+            v-for="day in days"
+            :key="day"
+            class="border border-gray-300 p-2 text-center font-semibold text-gray-700"
+        >
+          {{ formatMobile($t(day)) }}
+        </th>
+      </tr>
+      </thead>
+      <tbody class="relative">
+      <div class="absolute w-full" style="height: 1px; background: red" :style="offset <= 100 ? 'top: '+offset+'%' : 'display: none'"></div>
+      <tr
+          v-for="hour in hours"
+          :key="hour"
+          class="hover:bg-gray-50 transition-colors"
+      >
+        <td v-if="!isMobile" class="border border-gray-300 p-2 text-center font-medium text-sm bg-gray-50">
+          {{ formatHour(hour) }}
+        </td>
+        <td
+            v-for="day in days"
+            :key="day"
+            class="border border-gray-300 p-2 align-top h-16"
+        >
+          <div
+              class="space-y-1"
+              v-if="schedule[day][hour] && schedule[day][hour].length"
+          >
+            <div
+                class="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded shadow-sm"
+                v-for="event in schedule[day][hour]"
+                :key="event.id"
+            >
+              {{ formatMobile(event.name) }}
+            </div>
           </div>
-        </div>
-        <div v-else>
-        </div>
-      </div>
-    </div>
+          <div v-else class="h-full"></div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
   </div>
 </div>
 </template>
@@ -74,8 +102,30 @@ function formatDate(date) {
   return date.toISOString().split('T')[0]
 }
 
+function formatHour(hour: number) {
+  return (hour.toString().length == 1 ? '0' + hour : hour) + ':00'
+}
+
+function formatMobile(text: string) {
+  if(!isMobile)
+    return text
+
+  return text.length <= 3 ? text : text.substring(0, 3) + "."
+}
+
+const offset = ref(0);
+
 onMounted(() => {
   const today = new Date()
+
+  const hour = new Date().getHours();
+  const min = new Date().getMinutes();
+
+  const max = scheduleStore.maxHour - scheduleStore.minHour
+
+  let now = (hour + (min / 60)) - scheduleStore.minHour
+  offset.value = (now * 100) / max
+
   const day = today.getDay()
   const diffToMonday = today.getDate() - day + (day === 0 ? -6 : 1)
   const diffToSunday = today.getDate() - day + (day === 0 ? 0 : 7)
@@ -87,8 +137,19 @@ onMounted(() => {
   dateRange.value = [formatDate(monday), formatDate(sunday)];
 
   scheduleStore.getSchedule(1, dateRange.value[0], dateRange.value[1]);
+
+  setInterval(() => {
+    const hour = new Date().getHours();
+    const min = new Date().getMinutes();
+
+    const max = scheduleStore.maxHour - scheduleStore.minHour
+
+    let now = (hour + (min / 60)) - scheduleStore.minHour
+    offset.value = (now * 100) / max
+  }, 60000)
 })
 
+const isMobile = useIsMobile()
 </script>
 
 <style scoped>
